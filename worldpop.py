ffc0a42002a6fe3cd852d9c9c4a0b8eb54943da0
Patrick Werner <kug3lblitz@protonmail.com>
2018-05-16 11:21:06 -0400

    map styling, countries categorized by pop

--- worldpop.py
+++ worldpop.py
@@ -1,6 +1,7 @@
 import json
 from country_codes import get_country_code
 import pygal
+from pygal.style import LightColorizedStyle as LCS, RotateStyle as RS
 
 # load the data into a list
 filename = 'population_data.json'
@@ -18,8 +19,24 @@ for pop_dict in pop_data:
         if code:
             cc_populations[code] = population
 
-wm = pygal.maps.world.World()
+# group the countries into 3 population levels
+cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
+for cc, pop in cc_populations.items():
+    if pop < 10000000:
+        cc_pops_1[cc] = pop
+    elif pop < 1000000000:
+        cc_pops_2[cc] = pop
+    else:
+        cc_pops_3[cc] = pop
+
+# see how many countries are in each tier
+print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
+
+wm_style = RS('#336699', base_style=LCS)
+wm = pygal.maps.world.World(style=wm_style)
 wm.title = 'Word Population in 2010, by Country'
-wm.add('2010', cc_populations)
+wm.add('0-10m', cc_pops_1)
+wm.add('10m-1bn', cc_pops_2)
+wm.add('>1bn', cc_pops_3)
 
 wm.render_to_file('worldpopulation.svg')
