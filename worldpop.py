8f1de98cefa6669915365fee067015ebed058eb5
Patrick Werner <kug3lblitz@protonmail.com>
2018-05-16 10:13:40 -0400

    render worldmap w/ pop data

--- worldpop.py
+++ worldpop.py
@@ -1,12 +1,14 @@
 import json
 from country_codes import get_country_code
+import pygal
 
 # load the data into a list
 filename = 'population_data.json'
 with open(filename) as f:
     pop_data = json.load(f)
 
-# print the 2010 population for each country
+# build a dictionary of population data
+cc_populations = {}
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
@@ -14,6 +16,10 @@ for pop_dict in pop_data:
         code = get_country_code(country_name)
         population = int(float(pop_dict['Value']))
         if code:
-            print(code + ": "+ str(population))
-        else:
-            print('ERROR - ' + country_name)
+            cc_populations[code] = population
+
+wm = pygal.maps.world.World()
+wm.title = 'Word Population in 2010, by Country'
+wm.add('2010', cc_populations)
+
+wm.render_to_file('worldpopulation.svg')
